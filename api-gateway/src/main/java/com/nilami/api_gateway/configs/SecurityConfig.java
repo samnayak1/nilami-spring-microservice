01cb93b299712d65af827f89910c62899a4934de
Samasth Nayak <samasthnayak1@gmail.com>
2025-09-15 15:47:34 +0000

    keycloak fixes

--- api-gateway/src/main/java/com/nilami/api_gateway/configs/SecurityConfig.java
+++ api-gateway/src/main/java/com/nilami/api_gateway/configs/SecurityConfig.java
@@ -3,22 +3,24 @@ package com.nilami.api_gateway.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity  
 public class SecurityConfig {
+    
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated()
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().authenticated() 
             )
             .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults()) 
-            );
-        return http.build();
+                .jwt(Customizer.withDefaults())
+            )
+            .build();
     }
 }
